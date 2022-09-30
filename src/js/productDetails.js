eb9b2ab1d9782d6b5f53ea76107aa4e72b3fafdc
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-09-30 13:48:37 -0600

    team activity 2

---
+++ src/js/productDetails.js
@@ -0,0 +1,19 @@
+export default class ProductDetails {
+    constructor(productId, dataSource){
+      this.productId = productId;
+      this.product = {};
+      this.dataSource = dataSource;
+    }
+
+    init() {
+
+    }
+
+    addToCart() {
+
+    }
+
+    renderProductDetails() {
+        
+    }
+}
