18493c26d4b977a6b735ef6b3908b328f51c92e3
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-10-01 14:30:17 -0600

    finished stretch 1. ready to merge

--- src/js/product.js
+++ src/js/product.js
@@ -2,6 +2,7 @@ import ProductData from "./productData.js";
 import { getParams } from "./utils.js";
 import ProductDetails from "./productDetails.js";
 
+
 const dataSource = new ProductData('tents');
 const productId = getParams('product')
 const product = new ProductDetails(productId, dataSource)
