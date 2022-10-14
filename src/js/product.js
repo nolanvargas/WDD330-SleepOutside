35cfa22b2160962e39a2c7b401992407ab3a2c10
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2022-10-14 15:01:40 -0600

    fixed product.js

--- src/js/product.js
+++ src/js/product.js
@@ -6,13 +6,3 @@ const dataSource = new ProductData('tents');
 const productId = getParams('product');
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
-function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
-// add to cart button event handler
-function addToCart(e) {
-  const product = products.find((item) => item.Id === e.target.dataset.id);
-  setLocalStorage('so-cart', product);
-}
