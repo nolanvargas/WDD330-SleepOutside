557c05dbf8e8da12750464f56fb0e3f5f54b0c2c
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-01-30 16:28:53 -0700

    moved all files from build here

--- src/js/main.js
+++
@@ -1,5 +0,0 @@
-import { loadHeaderFooter } from "./utils.js";
-import Alert from "./alert.js";
-loadHeaderFooter();
-const alert = new Alert();
-alert.init();
--- src/js/product.js
+++
@@ -1,9 +0,0 @@
-import ExternalServices from "./ExternalServices.js";
-import ProductDetails from "./productDetails.js";
-import { getParam } from "./utils.js";
-
-const productId = getParam("product");
-const dataSource = new ExternalServices();
-
-const product = new ProductDetails(productId, dataSource);
-product.init();
